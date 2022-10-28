e73f43c5c8930faf23cc9f4eeaa9b52e3204a616
surrealgee <ariasacosta94@gmail.com>
2022-10-28 13:08:41 -0400

    Build initial UI, Refactor game engine

--- main.js
+++ main.js
@@ -1,55 +1,105 @@
+// 0. Variables
+
 let player = 0;
 let computer = 0;
+let totalMatches = 0;
+const startEl = document.getElementById("start-el");
+const rockBtn = document.getElementById("rock-btn");
+const paperBtn = document.getElementById("paper-btn");
+const scissorsBtn = document.getElementById("scissors-btn");
+const resultsEl = document.getElementById("results-el");
+const winnerEl = document.getElementById("winner-el");
+const playerEl = document.getElementById("player-el");
+const computerEl = document.getElementById("computer-el");
+
+// 1.Event Listeners
+
+startEl.addEventListener("click", function () {
+    game();
+});
+
+rockBtn.addEventListener("click", function () {
+    playRound("rock");
+});
+
+paperBtn.addEventListener("click", function () {
+    playRound("paper");
+});
+
+scissorsBtn.addEventListener("click", function () {
+    playRound("scissors");
+});
+
+// 2. Functions
+
+function game() {
+    totalMatches = 0;
+    player = 0;
+    computer = 0;
+    resultsEl.textContent = "The battle has started. Choose your Attack!";
+    playerEl.textContent = "";
+    computerEl.textContent = "";
+    winnerEl.textContent = "";
+    checkGame();
+}
+
+function hideBtns() {
+    rockBtn.classList.add("hidden");
+    paperBtn.classList.add("hidden");
+    scissorsBtn.classList.add("hidden");
+} 
+
+function checkGame() {
+    if (player == 5 || computer == 5) {
+
+        hideBtns();
+
+        if (player > computer) {
+            winnerEl.textContent = `Player: ${player} vs Computer: ${computer}. Player Wins!.`;
+        } else if (computer > player) {
+            winnerEl.textContent = `Player: ${player} vs Computer: ${computer}. Computer Wins!.`;
+        } else {
+            winnerEl.textContent = `Player: ${player} vs Computer: ${computer}. Draw.`;
+        }
+    } else if (totalMatches > 0) {
+        winnerEl.textContent = `Player: ${player} vs Computer: ${computer}.`
+    }
+}
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = getComputerChoice();
+    playerSelection = playerSelection.toLowerCase();
+    playerEl.textContent = playerSelection;
+    computerEl.textContent = computerSelection;
+
+    if (playerSelection === computerSelection) {
+        resultsEl.textContent = "Draw.";
+        totalMatches++;
+    } else if (playerSelection === "rock"
+        && computerSelection === "scissors"
+        || playerSelection === "paper"
+        && computerSelection === "rock"
+        || playerSelection === "scissors"
+        && computerSelection === "paper") {
+        player++;
+        totalMatches++;
+        resultsEl.textContent = `You've Won! ${playerSelection} beats ${computerSelection}.`;
+    } else {
+        computer++;
+        totalMatches++
+        resultsEl.textContent = `You Lose! ${computerSelection} beats ${playerSelection}.`;
+    }
+    checkGame();
+}
 
 function getComputerChoice() {
     let chooser = Math.floor(Math.random() * 99) + 1;
-    
+
     if (chooser <= 33) {
         return "rock";
     } else if (chooser <= 66) {
         return "paper";
     } else {
         return "scissors";
-    }
-}
-
-function playRound(playerSelection, computerSelection) {
-    computerSelection = getComputerChoice();
-    playerSelection = prompt("Choose!", );
-    playerSelection = playerSelection.toLowerCase();
-    console.log(playerSelection);
-    console.log(computerSelection); 
-
-    if (playerSelection === computerSelection) {
-        console.log("Draw");
-    } else if (playerSelection === "rock" 
-    && computerSelection === "scissors" 
-    || playerSelection === "paper" 
-    && computerSelection === "rock"
-    || playerSelection === "scissors" 
-    && computerSelection === "paper" ) {
-        player++;
-        console.log(`You've Won! ${playerSelection} beats ${computerSelection}`);        
-    } else {
-        computer++;
-        console.log(`You Lose! ${computerSelection} beats ${playerSelection}`);
-    }
-}
-
-function game() {
-    player = 0;
-    computer = 0;
-
-    for (let i = 0; i < 5; i++) {
-        playRound();
-        console.log(`Player: ${player}, Computer: ${computer}.`);
-    }
-
-    if (player > computer) {
-        return "Player Wins";
-    } else if (computer > player) {
-        return "Computer Wins";
-    } else {
-        return "Draw";
     }
 }
